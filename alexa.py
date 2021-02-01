f0ed6ad8cf9d7d7eb3c98b16009cbc995bb27d7b
Archit Sharma <architgwl2000@gmail.com>
2021-02-01 23:53:35 +0530

    added code

---
+++ alexa.py
@@ -0,0 +1,31 @@
+import pyttsx3
+
+#initialisation
+engine = pyttsx3.init()
+
+#newVoiceRate = 50
+sen = '.......Welcome to Our Project on playing music using speech recognition. '
+
+#engine processes
+engine.say(sen)
+engine.runAndWait()
+# get audio from microphone
+
+import speech_recognition as sr
+from playsound import playsound
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Currently available songs are : Sunny Sunny,diamond,dil wali gal, daru badnam,hindi song. ")
+    print("which song you wanna listen:")
+    audio = r.listen(source)
+
+# get the text from audio
+msg = r.recognize_google(audio)
+
+song=msg+'.mp3'
+
+print(song)
+
+playsound(song)
